3e0573bbe8f0e80684a057e2a5e5e714b770ec31
Ikram-Ali96 <ikram.abdullahi25@gmail.com>
2025-03-17 13:37:42 +0000

    Adding more JavaScript

--- src/script.js
+++ src/script.js
@@ -17,6 +17,8 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  displayForecast(response.data.city);
 }
 function formateDate(date) {
   let minutes = date.getMinutes();
@@ -49,8 +51,15 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function getForecast(city) {
+let apiKey = "d6973oc76fcf3c7at603c4b3a18b2464";
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
 
-function displayForecast() {
+
+function displayForecast(response) {
+console.log(response.data);
 
 
 let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -82,4 +91,4 @@ forecastElement.innerHTML = forecastHtml;
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("London");  
-displayForecast();
+getForecast("London");
