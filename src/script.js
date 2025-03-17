5ee7ab7e9e7fb36bcd8e2c63e079bf25a550e87b
Ikram-Ali96 <ikram.abdullahi25@gmail.com>
2025-03-17 14:20:26 +0000

    Update Script JS

--- src/script.js
+++ src/script.js
@@ -67,7 +67,7 @@ axios.get(apiUrl).then(displayForecast);
 function displayForecast(response) {
     let forecastHtml = "";
 
-response.day.daily.forEach(function (day, index) {
+response.data.daily.forEach(function (day, index) {
     if (index < 5) {
     forecastHtml =
      forecastHtml +
